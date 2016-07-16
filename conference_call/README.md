ff8b9308660b571189d9ae67ac35e30f7ff70f58
Đức Nguyễn Huy <c Nguyn Huy>
2016-07-16 10:57:21 +0700

    Upload new file server

--- conference_call/README.md
+++ conference_call/README.md
@@ -1,1 +1,53 @@
-{"filter":false,"title":"README.md","tooltip":"/conference_call/README.md","undoManager":{"mark":-1,"position":-1,"stack":[]},"ace":{"folds":[],"scrolltop":60,"scrollleft":0,"selection":{"start":{"row":19,"column":77},"end":{"row":19,"column":96},"isBackwards":true},"options":{"guessTabSize":true,"useWrapMode":false,"wrapToView":true},"firstLineState":{"row":3,"state":"start","mode":"ace/mode/markdown"}},"timestamp":1467801503603,"hash":"f1a17403204e7c9fdfa09515262a49255603c46d"}
+Mobile Quickstart
+===
+
+This repository is located at [https://github.com/twilio/mobile-quickstart](https://github.com/twilio/mobile-quickstart).
+
+This repository includes server-side web application required for Twilio mobile sample apps such as Quickstart and BasicPhone. The Python script is responsible for generating capability tokens and serving TwiML.
+
+Prerequisites
+---
+
+Please [sign up](https://www.twilio.com/try-twilio) for a free Twilio account. You will need your Account Sid and Auth Token available in your [Account Dashboard](https://www.twilio.com/user/account/). You will also need to create a [TwiML App](https://www.twilio.com/user/account/apps). Please make sure that you point the TwiML app's Voice Request URL to your web application's public URL, e.g.,  http://companyfoo.com/call.
+You will also need a verified phone number to be used as caller ID when placing phone calls.  See the "Verified Caller Ids" section
+under [Numbers](https://www.twilio.com/user/account/phone-numbers)
+tab in your Twilio Account.
+
+Deployment
+---
+
+In order to Twilio can communicate with your web application, it needs to be
+deployed on the public Internet.  Some of the options include using [Heroku](https://heroku.com/), [ngrok](https://ngrok.com/), etc.
+
+Click the button below to automatically set up the app using your Heroku account. Please enter your Twilio Account Sid, Auth token, TwiML App Sid and verified phone number when prompted.
+
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
+
+If you prefer to run your application locally, please make sure that you have Python and `pip` installed. Please install the required packages:
+
+    pip install -r requirements.txt
+
+Please replace `ACCOUNT_SID`, `AUTH_TOKEN`, `APP_SID` and `CALLER_ID` in `server.py` with values from your Twilio account.  Simply run the application by `python server.py`.  You can tunnel `localhost` to the public Internet using `ngrok`: 
+
+    ngrok http 5000
+
+Testing
+---
+
+Please open http://companyfoo.com/token in your browser. You should see a long string.
+
+
+Client Configuration
+---
+
+Please modify BasicPhone and Quickstart to point to your web applications's public URL.
+
+
+Usage
+---
+
+At launch, the client connects to http://companyfoo.com/token to retrieve its capability token.
+
+When you hit the "Call" button in the mobile app, it makes a call to your Twilio application that then makes a request to your web server (http://companyfoo.com/call).  The script routes the call accordingly, either to a phone number or to another client. 
+
+If you want to receive a phone call in your mobile app you need to have a Twilio phone number (https://www.twilio.com/user/account/phone-numbers/incoming).  Please configure the phone number's Voice Request URL to point to your web application's public URL (http://companyfoo.com/call). When someone calls your Twilio phone number, Twilio makes a request to your web application. Your web application returns TwiML that instructs Twilio to connect the phone call with client `jenny`. Either a purchased number or your sandbox number can be used.
--- conference_call/server.py
+++ conference_call/server.py
@@ -1,1 +1,78 @@
-{"filter":false,"title":"server.py","tooltip":"/conference_call/server.py","undoManager":{"mark":2,"position":2,"stack":[[{"start":{"row":10,"column":11},"end":{"row":10,"column":45},"action":"remove","lines":["AP697cd69c6698315d628f67c90fa9b7da"],"id":2},{"start":{"row":10,"column":11},"end":{"row":10,"column":45},"action":"insert","lines":["AP697cd69c6698315d628f67c90fa9b7da"]}],[{"start":{"row":6,"column":15},"end":{"row":6,"column":49},"action":"remove","lines":["AC2ec29e448b2171dccbcb1746ea13badc"],"id":3},{"start":{"row":6,"column":15},"end":{"row":6,"column":49},"action":"insert","lines":["AC2ec29e448b2171dccbcb1746ea13badc"]}],[{"start":{"row":7,"column":14},"end":{"row":7,"column":46},"action":"remove","lines":["65e701e3de0d9db5f219f8bcbbcfd91e"],"id":4},{"start":{"row":7,"column":14},"end":{"row":7,"column":46},"action":"insert","lines":["65e701e3de0d9db5f219f8bcbbcfd91e"]}]]},"ace":{"folds":[],"scrolltop":942,"scrollleft":0,"selection":{"start":{"row":78,"column":0},"end":{"row":78,"column":0},"isBackwards":false},"options":{"guessTabSize":true,"useWrapMode":false,"wrapToView":true},"firstLineState":{"row":66,"state":"start","mode":"ace/mode/python"}},"timestamp":1468032462858,"hash":"9807e78420723e57f5d5e7771800fc5359f643b7"}
+import os
+from flask import Flask, request
+from twilio.util import TwilioCapability
+import twilio.twiml
+
+# Account Sid and Auth Token can be found in your account dashboard
+ACCOUNT_SID = 'AC2ec29e448b2171dccbcb1746ea13badc'
+AUTH_TOKEN = '65e701e3de0d9db5f219f8bcbbcfd91e'
+
+# TwiML app outgoing connections will use
+APP_SID = 'AP697cd69c6698315d628f67c90fa9b7da'
+
+CALLER_ID = '+16787078778'
+CLIENT = 'jenny'
+
+app = Flask(__name__)
+
+@app.route('/token')
+def token():
+  account_sid = os.environ.get("ACCOUNT_SID", ACCOUNT_SID)
+  auth_token = os.environ.get("AUTH_TOKEN", AUTH_TOKEN)
+  app_sid = os.environ.get("APP_SID", APP_SID)
+
+  capability = TwilioCapability(account_sid, auth_token)
+
+  # This allows outgoing connections to TwiML application
+  if request.values.get('allowOutgoing') != 'false':
+     capability.allow_client_outgoing(app_sid)
+
+  # This allows incoming connections to client (if specified)
+  client = request.values.get('client')
+  if client != None:
+    capability.allow_client_incoming(client)
+
+  # This returns a token to use with Twilio based on the account and capabilities defined above
+  return capability.generate()
+
+@app.route('/call', methods=['GET', 'POST'])
+def call():
+  """ This method routes calls from/to client                  """
+  """ Rules: 1. From can be either client:name or PSTN number  """
+  """        2. To value specifies target. When call is coming """
+  """           from PSTN, To value is ignored and call is     """
+  """           routed to client named CLIENT                  """
+  resp = twilio.twiml.Response()
+  from_value = request.values.get('From')
+  to = request.values.get('To')
+  if not (from_value and to):
+    resp.say("Invalid request")
+    return str(resp)
+  from_client = from_value.startswith('client')
+  caller_id = os.environ.get("CALLER_ID", CALLER_ID)
+  from_conference = from_value.startswith('conference')
+  from_value = request.values.get('From')
+  if to.startswith("conference:"):
+    # Client to conference
+    resp = twilio.twiml.Response()
+    resp.dial(callerId=from_value).conference(from_value)
+  elif not from_client:
+    # PSTN -> client
+    resp.dial(callerId=from_value).client(CLIENT)
+  elif to.startswith("client:"):
+    # client -> client
+    resp.dial(callerId=from_value).client(to[7:])
+  else:
+    # client -> PSTN
+    resp.dial(to, callerId=caller_id)
+  return str(resp)
+
+@app.route('/', methods=['GET', 'POST'])
+def welcome():
+  resp = twilio.twiml.Response()
+  resp.say("Welcome to Twilio")
+  return str(resp)
+
+if __name__ == "__main__":
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host='0.0.0.0', port=port, debug=True)
